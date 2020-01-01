85f2d9bec2b0ad8f860a129b0e7b59ebb655537a
Joshua Wade <SecondFlight@users.noreply.github.com>
2019-12-31 21:33:27 -0500

    add malloc/

---
+++ malloc/precache-manifest.01584e16243c9f01c2b3cafb59b41b57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39654fafbf92c84bf51e32a108f126ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc13ed6a1b8f618451c",
+    "url": "/static/css/main.9372133b.chunk.css"
+  },
+  {
+    "revision": "ef53989c515e8ef913ca",
+    "url": "/static/js/2.ce3fdcee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ce3fdcee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdc13ed6a1b8f618451c",
+    "url": "/static/js/main.7cd53e68.chunk.js"
+  },
+  {
+    "revision": "5916971886b32ca725e8",
+    "url": "/static/js/runtime-main.62381573.js"
+  }
+]);
